9ba016781c95239b59b9335a7a6026cec8a09386
TimGoransson <tim.goransson@gmail.com>
2016-05-16 12:23:37 +0200

    WORK!

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+    public int damage = 5;
+
+    public float speed;
+    public float fallBoundry = -20;
+    public float Timer = 2;
+    
+    public Rigidbody2D rb;
+	void Start ()
+    {
+        rb.velocity = new Vector2(speed * rb.velocity.x, speed * rb.velocity.y);
+    }
+	
+	void Update ()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            Destroy(gameObject);
+            Timer = 2;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.SendMessageUpwards("Damage", damage);
+            Destroy(gameObject);
+
+        }
+        else if (other.tag != "Enemy")
+        {
+            Destroy(gameObject);
+        }
+        //Destroy(other.gameObject);
+    }
+}
